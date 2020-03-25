ebe383c9c29167f27c378fa07bdefa78836c3864
529659435 <529659435@qq.com>
2020-03-25 21:39:58 +0800

    功能 前端-实现未登录后跳转回首页

--- pjf/src/main/resources/static/js/community.js
+++ pjf/src/main/resources/static/js/community.js
@@ -1,24 +1,33 @@
 function post() {
-    var questionId =$("#question_id").val();
+    var questionId = $("#question_id").val();
     var content = $("#comment_coneent").val();
-   $.ajax({
-       type:"POST",
-       url:"/comment",
-       contentType:'application/json',
-       data: JSON.stringify({
-           "parentId":questionId,
-           "content":content,
-           "type":1
-       }),
-       success:function (response) {
-           if(response.code == 200){
-               $("#comment_coneent").hide();
-           }else {
-               alert(response.message);
-           }
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if (response.code == 200) {
+                $("#comment_coneent").hide();
+            } else {
+                //实现提示未登录，点击登录后，跳转回首页
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=3aa17e57a080a05be3fa&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
             console.log(response);
-       },
-       dataType:"json"
+        },
+        dataType: "json"
 
-   });
+    });
 }
